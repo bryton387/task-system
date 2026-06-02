f3d62a58b8d1a021d6b4add4dae0b2cd44aec47a
Bryton <bryton387m@gmail.com>
2026-06-02 21:26:20 +0300

    Export task manager helpers

--- task_manager/__init__.py
+++ task_manager/__init__.py
@@ -1,1 +1,17 @@
 """Task management package."""
+
+from .task_utils import (
+    add_task,
+    calculate_progress,
+    get_progress,
+    mark_completed,
+    view_pending_tasks,
+)
+
+__all__ = [
+    "add_task",
+    "calculate_progress",
+    "get_progress",
+    "mark_completed",
+    "view_pending_tasks",
+]
